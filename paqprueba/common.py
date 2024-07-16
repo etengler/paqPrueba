7f7dccdaa0e0096c48a9395554d3acf3996d24d3
Evelyn Tengler <tenglerevelyn@gmail.com>
2024-07-16 11:06:53 -0300

    agregar funciones

--- paqprueba/common.py
+++ paqprueba/common.py
@@ -5,3 +5,27 @@ def hello_world():
     """Prints "Hello World!" to the console.
     """
     print("Hello World!")
+    
+    
+    
+    
+def get_version():
+    """Returns the version of the paqprueba package.
+    
+    Returns:
+        str: The version of the paqprueba package.
+    """
+    
+    return "0.0.1"
+
+
+def random_number():
+    """Return a random number between 0 and 1.
+
+    Returns:
+        float: A random number between 0 and 1.
+    """
+    
+    import random
+    return random.random()
+  
