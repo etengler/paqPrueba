befec9c7f5124d3a4168cf7dd166a9345002418f
Evelyn Tengler <tenglerevelyn@gmail.com>
2024-09-11 14:34:50 -0300

    cosas

--- paqprueba/paqprueba.py
+++ paqprueba/paqprueba.py
@@ -3,9 +3,21 @@
 import ipyleaflet
 
 class Map(ipyleaflet.Map):
-    
+    """Esta es la clase map que hereda de ipyleaflet.Map.
+
+    Args:
+        ipyleaflet (Map): La clase ipyleaflet.Map.
+    """    
     
     def __init__(self, center= [20, 0], zoom=2, **kwargs):
+        """Inicializa el mapa
+
+        Args:
+            center (list, optional): Setea el centro del mapa. Por default toma estos valores [20, 0].
+            zoom (int, optional): Setea el zoom del mapa. Por default toma el valor 2.
+        """        
         super().__init__(center=center, zoom=zoom, **kwargs)
         self.add_control(ipyleaflet.LayersControl())
-        
+
+
+   
---
+++ paqprueba/utils.py
@@ -0,0 +1,18 @@
+"""Este modulo contiene las funciones utility para paqprueba
+"""
+
+def csv_to_df(csv_file):
+    """Convierte un CSV en un pandas data frame
+
+    Args:
+        csv_file (str): la direccion del archivo csv
+    """    
+    import pandas as pd
+    
+    return pd.read_csv(csv_file)
+
+
+     
+
+    
+    
